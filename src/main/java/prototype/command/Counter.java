72a90d191c3240d4b961a526ca3cbf0693a198c9
André Rouél <arouel@datameer.com>
2018-09-18 18:33:19 +0200

    Add command example

---
+++ src/main/java/prototype/command/Counter.java
@@ -0,0 +1,22 @@
+package prototype.command;
+
+import org.immutables.gson.Gson.TypeAdapters;
+import org.immutables.mongo.Mongo.Id;
+import org.immutables.mongo.Mongo.Repository;
+import org.immutables.value.Value.Immutable;
+
+@Immutable
+@Repository("counters")
+@TypeAdapters
+abstract class Counter {
+
+    static Counter of(String collection, long sequence) {
+        return ImmutableCounter.builder().collection(collection).sequence(sequence).build();
+    }
+
+    @Id
+    abstract String collection();
+
+    abstract long sequence();
+
+}
